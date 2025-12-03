d305faa958a03a5bb314af77cd70e9a331b74148
esu6 <143674322+esu6@users.noreply.github.com>
2025-12-03 15:17:08 -0600

    new clean data file

---
+++ clean_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import math
+import numpy as np
+# from langdetect import detect
+import re
+
+df = pd.read_csv('Books_df.csv')
+
+# get rid of price column
+df.drop(columns=['Price'], inplace=True)
+
+# remove duplicate entries 
+df.drop_duplicates()
+
+# remove unneeded types (not actually books)
+df = df[df['Type'] != 'Map']
+df = df[df['Type'] != 'Game']
+df = df[df['Type'] != 'Calendar']
+df = df[df['Type'] != 'Stationery']
+df = df[df['Type'] != 'Workbook']
+df = df[df['Type'] != 'Diary']
+df = df[df['Type'] != 'Misc. Supplies']
+df = df[df['Type'] != 'Cards']
+df = df[df['Type'] != 'Poster']
+df = df[df['Type'] != 'Gift']
+df = df[df['Type'] != 'Single Issue Magazine']
+df = df[df['Type'] != 'Puzzle']
+df = df[df['Type'] != 'Wall Chart']
+df = df[df['Type'] != 'Sheet music']
+df = df[df['Type'] != 'Card Book']
+
+# combine main genre + sub genre columns
+df['Genre'] = df['Main Genre'] + ' ' + df['Sub Genre']
+df = df.drop(columns=['Main Genre'])
+df = df.drop(columns=['Sub Genre'])
+
+# properly reorder columns
+order = df.columns.tolist()
+order.remove('Genre')
+order.insert(3, 'Genre')
+
+df = df[order]
+
+# upload to new csv vile 
+df.to_csv('cleaned_data.csv', index=False)
