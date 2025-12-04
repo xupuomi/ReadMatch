912e3197d00360b819c4936265575a299aad86ff
esu6 <esu6@illinois.edu>
2025-12-03 21:40:46 -0600

    update cleaned data

--- clean_data.py
+++ clean_data.py
@@ -4,17 +4,54 @@ import numpy as np
 # from langdetect import detect
 import re
 import os
+from collections import OrderedDict
+
+def clean_title(title):
+    # Convert to lowercase
+    title = str(title).lower()
+    
+    # Remove everything after common subtitle separators (:, -, |)
+    title = re.split(r'[:—\-|]', title)[0].strip()
+    
+    # Remove common book descriptions/identifiers (e.g., 'ebook', 'bestseller')
+    title = re.sub(r'\b(the|a|an|copy|ebook|bestseller|novel|edition|paperback|hardcover)\b', '', title)
+    
+    # Remove extra spaces
+    title = re.sub(r'\s+', ' ', title).strip()
+    
+    return title
+
+def standardize_author(name):
+    name_parts = str(name).lower().split()
+    return ' '.join(sorted(name_parts))
+
+def clean_genre(genre): 
+    genre = re.sub(r'[&,]', ' ', genre).strip()
+    genre = genre.split()
+    
+    genre = set(OrderedDict.fromkeys(genre)) 
+    genre = list(genre) 
+    genre = " ".join(genre)
+    return genre
+
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 csv_file_path = os.path.join(base_dir, "Books_df.csv")
 
 df = pd.read_csv(csv_file_path)
+df.columns = df.columns.str.strip()
 
 # get rid of price column
 df.drop(columns=['Price'], inplace=True)
 
-# remove duplicate entries 
-df.drop_duplicates()
+# remove exact duplicate entries 
+df.drop_duplicates(subset=['Title'], inplace=True)
+df['Clean_Title'] = df['Title'].apply(clean_title)
+df['Clean_Author'] = df['Author'].apply(standardize_author)
+
+df.drop_duplicates(subset=['Clean_Title'], inplace=True)
+df.drop_duplicates(subset=['Clean_Author'], inplace=True)
+
 
 # remove unneeded types (not actually books)
 df = df[df['Type'] != 'Map']
@@ -32,6 +69,11 @@ df = df[df['Type'] != 'Puzzle']
 df = df[df['Type'] != 'Wall Chart']
 df = df[df['Type'] != 'Sheet music']
 df = df[df['Type'] != 'Card Book']
+# df = df[df['Type'] != 'Kindle Edition']
+
+
+df.sort_values(by='No. of People rated', ascending=False, inplace=True)
+df = df.iloc[:4000]
 
 # combine main genre + sub genre columns
 df['Genre'] = df['Main Genre'] + ' ' + df['Sub Genre']
@@ -41,9 +83,16 @@ df = df.drop(columns=['Sub Genre'])
 # properly reorder columns
 order = df.columns.tolist()
 order.remove('Genre')
+order.remove('Clean_Title')
+order.remove('Clean_Author')
+
 order.insert(3, 'Genre')
+order.insert(1, 'Clean_Title')
+order.insert(2, 'Clean_Author')
 
 df = df[order]
+
+df['Genre'] = df['Genre'].apply(clean_genre)
 
 # upload to new csv vile 
 df.to_csv('cleaned_data.csv', index=False)
